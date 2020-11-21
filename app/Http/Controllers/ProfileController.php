3f7909c5f1944b3175a9d3cea40abe12fb1c1a2c
latami12 <ppib14latami@gmail.com>
2020-11-21 09:07:45 +0700

    initial commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Auth;
 use Alert;
 use App\User;
@@ -11,28 +12,27 @@ class ProfileController extends Controller
 {
     public function index()
     {
-     $user = User::where('id', Auth::user()->id)->first();
-  return $this->sendResponse('Succes', 'Ini profile anda', $user, 200);
-     // return view('profile.index', compact('user'));
+        $user = User::where('id', Auth::user()->id)->first();
+        return $this->sendResponse('Succes', 'Ini profile anda', [$user], 200);
+        // return view('profile.index', compact('user'));
     }
 
     public function update(Request $request)
     {
-      $this->validate($request, [
+        $this->validate($request, [
             'password'  => 'confirmed',
         ]);
 
-     $user = User::where('id', Auth::user()->id)->first();
-     $user->name = $request->name;
-     $user->email = $request->email;
-     $user->nomor_telpon = $request->nomor_telpon;
-     $user->alamat = $request->alamat;
-     if(!empty($request->password))
-     {
-      $user->password = Hash::make($request->password);
-     }
-     
-     $user->update();
-        return $this->sendResponse('Success', 'profile anda telah di upgarde    ', $user, 200);
+        $user = User::where('id', Auth::user()->id)->first();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->nomor_telpon = $request->nomor_telpon;
+        $user->alamat = $request->alamat;
+        if (!empty($request->password)) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->update();
+        return $this->sendResponse('Success', 'profile anda telah di upgrade', [$user], 200);
     }
 }
